416a4cddbccb732cc17ac6fdb958b042ac24ecc9
D Curiel <curieldaniel7@gmail.com>
2022-07-10 16:25:47 +0200

    Unity Tutorial about a stealth game
    Full game completed

---
+++ jhon_lemon/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    [SerializeField]
+    float fadeDuration = 5f;
+
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    CanvasGroup exitBackgroundImageCanvasGroup;
+    [SerializeField]
+    CanvasGroup caughtBackgroundImageCanvasGroup;
+    [SerializeField] 
+    float displayImageDuration = 1f;
+    [SerializeField]
+    AudioSource exitAudio;
+    [SerializeField]
+    AudioSource caughtAudio;
+
+    bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
+    float m_Timer; 
+    bool m_HasAudioPlayed;
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    private void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+
+    }
+}
